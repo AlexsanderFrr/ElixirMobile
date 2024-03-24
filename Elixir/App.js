5d8c24c3369126f47cdb2f5aff898a2230bc3af3
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-03-24 17:09:46 -0300

    background

--- Elixir/App.js
+++ Elixir/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Elixir Natural</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F4DEAA',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30, // Tamanho da fonte
+    color: '#BB5104', // Cor do texto (exemplo: preto)
   },
 });
