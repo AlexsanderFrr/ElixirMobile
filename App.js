a619d123e7630341f1c06c9b59d5d240918268d3
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-03-30 21:16:07 -0300

    Alteração no nome

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./components/Home";
-import Splash from "./components/Splash";
+import Splash from "./components/splash";
 const Stack = createStackNavigator();
 
 export default function App() {
