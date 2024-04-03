020c8f1473fa313be2f6d6ae21d14231f5168abd
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-04-03 11:24:56 -0300

    Rotas e navegações funcionando

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+
 import LoginScreen from "./components/LoginScreen";
 import SplashScreen from "./components/SplashScreen";
 import HomeScreen from "./components/HomeScreen";
 import CadastroScreen from "./components/CadastroScreen";
 import Routes from "./src/routes";
 
-
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
