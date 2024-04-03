f2e8b7b5de8b9626d221ffc8aa1f1077ccfe8307
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-04-03 15:39:36 -0300

    ajustments and shopscreen.js

--- components/ShopScreen.js
+++ components/ShopScreen.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, KeyboardAvoidingView, StyleSheet } from 'react-native'
 
-export default function ShopScreen() {
+const ShopScreen = () => {
+
   return (
-    <View>
-      <Text>ShopScreen</Text>
-    </View>
+    <KeyboardAvoidingView style={[styles.container, styles.brownbg]}>
+      <View style={styles.text_header}>
+        <Text>Sacola de Suco</Text>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      paddingHorizontal: 30,
+      paddingTop: 60,
+    },
+    brownbg: {
+      backgroundColor: "#F4DEAA",
+    },
+    text_header: {
+
+    }
+})
+
+
+export default ShopScreen;
