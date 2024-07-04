46e2e39329856142a18e60352d86b587d2919bda
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-04 19:29:43 -0300

    Botões de navegação PerfilScreen

--- components/PerfilScreen.js
+++ components/PerfilScreen.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, Image, ScrollView } from 'react-native';
+import React from 'react';
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
 export default function PefilScreen() {
   return (
     <View>
-      <Text>PefilScreen</Text>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <View>
+            <Ionicons name='arrow-back' size={size} color={"#000000"}></Ionicons>
+            <Ionicons name='settings' size={size} color={"#000000"}></Ionicons>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
