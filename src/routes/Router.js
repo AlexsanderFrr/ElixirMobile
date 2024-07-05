003ae5645fa7c5d3312c549736d634b4d8f2d6b4
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-05 13:43:14 -0300

    Contextos de autenticação

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import StackRoutes from "./stack.routes";
+import AuthStack from "./auth.stack.routes";
 
 export default function Routes() {
+    const auth = false;
     return (
         <NavigationContainer>
-            <StackRoutes />
+            {auth ? <StackRoutes /> : <AuthStack/>}
         </NavigationContainer>
     )
 }
