47ba3a0e245fd16cac23480ff76afa9e6c93d468
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-05 00:55:22 -0300

    Ultimas alterações Tela de Perfil

--- components/PerfilScreen.js
+++ components/PerfilScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView, Image, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Image, ScrollView, TouchableOpacity, ImageBackground } from 'react-native';
 import React from 'react';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native';
@@ -8,49 +8,80 @@ const PerfilScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.titleBar}>
-          <Ionicons name='arrow-back' size={40} color={"#000000"} onPress={() => navigation.goBack()}></Ionicons>
-          <Ionicons name='settings-sharp' size={40} color={"#000000"}></Ionicons>
-        </View>
+      <ImageBackground source={require("../assets/fundoPerfil.png")} resizeMode="cover" style={styles.imageBG}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.titleBar}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name='arrow-back' size={40} color={"#fff"}></Ionicons>
+            </TouchableOpacity>
+            <Ionicons name='settings-sharp' size={40} color={"#fff"}></Ionicons>
+          </View>
 
-        <View style={{ alignSelf: "center" }}>
-          <Text style={styles.textMain}>Meu Perfil</Text>
-          <View style={styles.profileImage}>
-            <Image source={{ uri: 'https://img.freepik.com/fotos-premium/imagenslegais-e-calmas-para-a-foto-do-perfil-do-whatsapp-arte-gerada-porai_873370-5052.jpg' }} style={styles.image} resizeMode='center' />
+          <View style={{ alignSelf: "center" }}>
+            <Text style={styles.textMain}>Meu Perfil</Text>
+            <View style={styles.profileImage}>
+              <Image source={{ uri: 'https://img.freepik.com/fotos-premium/imagenslegais-e-calmas-para-a-foto-do-perfil-do-whatsapp-arte-gerada-porai_873370-5052.jpg' }} style={styles.image} resizeMode='center' />
+            </View>
+            <View style={styles.dm}>
+              <MaterialIcons name='chat' size={18} color={"#F4DEAA"}></MaterialIcons>
+            </View>
+            <View style={styles.active}></View>
+            <View style={styles.add}>
+              <Ionicons name='add' size={38} color={"#F4DEAA"}></Ionicons>
+            </View>
           </View>
-          <View style={styles.dm}>
-            <MaterialIcons name='chat' size={18} color={"#F4DEAA"}></MaterialIcons>
-          </View>
-          <View style={styles.active}></View>
-          <View style={styles.add}>
-            <Ionicons name='add' size={38} color={"#F4DEAA"}></Ionicons>
-          </View>
-        </View>
 
-        <View style={styles.infoContainer}> 
-          <Text style={[styles.text, { fontWeight: "200", fontSize: 26, marginTop: 40}]}>Alexsandra Ferreira</Text>
-          <View style={[styles.separator, { marginTop: 10 }]} />
-        </View>
-      </ScrollView>
+          <View style={styles.infoContainer}>
+            <Text style={[styles.text, { fontWeight: "600", fontSize: 26, marginTop: 30, fontFamily: "HelveticaNeue", }]}>Alexsandra Ferreira</Text>
+            <View style={[styles.separator, { marginTop: 10, width: "90%" }]} />
+
+            <View style={styles.groupOption}>
+              <TouchableOpacity>
+                <View style={[styles.option, { marginTop: 40 }]}>
+                  <Image source={require("../assets/iconDiag.png")}></Image>
+                  <Text style={[styles.text, { fontSize: 16, fontWeight: "600", marginLeft: 10 }]}>Meus Diagnósticos</Text>
+                </View>
+              </TouchableOpacity>
+              <View style={[styles.separator, { opacity: 0.1 }]} />
+              <TouchableOpacity>
+                <View style={[styles.option, { marginTop: 10 }]}>
+                  <Image source={require("../assets/iconCora.png")}></Image>
+                  <Text style={[styles.text, { fontSize: 16, fontWeight: "600", marginLeft: 10 }]}>Favoritos</Text>
+                </View>
+              </TouchableOpacity>
+              <View style={[styles.separator, { opacity: 0.1 }]} />
+              <TouchableOpacity>
+                <View style={[styles.option, { marginTop: 10 }]}>
+                  <Image source={require("../assets/iconLogOut.png")}></Image>
+                  <Text style={[styles.text, { fontSize: 16, fontWeight: "600", marginLeft: 10 }]}>Sair</Text>
+                </View>
+              </TouchableOpacity>
+              <View style={[styles.separator, { opacity: 0.1 }]} />
+            </View>
+          </View>
+        </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  imageBG: {
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: 'center'
   },
   textMain: {
+    color: "#fff",
     fontSize: 20,
     fontWeight: "700",
     textAlign: "center",
     marginBottom: 15
   },
   text: {
-    fontFamily: "HelveticaNeue",
-    color: "#52575D"
+    color: "#000000"
   },
   titleBar: {
     flexDirection: 'row',
@@ -104,17 +135,26 @@ const styles = StyleSheet.create({
   },
   separator: {
     height: 2,
-    width: "90%",
+    width: "100%",
     backgroundColor: "#000000"
   },
   infoContainer: {
-    backgroundColor: "#F5F5F5",
+    backgroundColor: "#fff",
     width: "90%",
-    height: "100%",
+    height: 550,
     alignSelf: "center",
     alignItems: "center",
-    marginTop: 20,
-    
+    marginTop: 15,
+    borderRadius: 15
+  },
+  option: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10
+  },
+  groupOption: {
+    alignItems: "flex-start",
+    width: "90%"
   }
 });
 
