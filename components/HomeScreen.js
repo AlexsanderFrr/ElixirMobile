fa575b3dcd631076e445829921aef5921b9810ff
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-16 20:39:21 -0300

    Commit provisório

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -51,6 +51,36 @@ const HomeScreen = () => {
     setFilteredJuices(filtered);
   };
 
+  const juices = [
+    {
+      image: require("../assets/garrafa-suco2.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco4.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco5.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco4.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco2.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco5.png"),
+    },
+    {
+      image: require("../assets/garrafa-suco.png"),
+    }
+  ];
+
   // Filtra os sucos com base na categoria selecionada
   const filteredCategoryJuices = () => {
     switch (selectedCategory) {
