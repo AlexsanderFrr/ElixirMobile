217b25cb6dd233990ae964eb94189f5eb7cedf25
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-18 13:48:03 -0300

    Imagem header e feedback de tempo

--- components/ExibicaoScreen.js
+++ components/ExibicaoScreen.js
@@ -13,6 +13,11 @@ const ExibicaoScreen = ({ route }) => {
     <View style={styles.container}>
       <StatusBar backgroundColor="#BB5104" />
 
+      <Image
+        source={require("../assets/sucodelimao.jpg")}
+        style={styles.imageHeader}
+        resizeMode="cover"
+      />
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Ionicons name='arrow-back' size={40} color={"#fff"}></Ionicons>
@@ -22,7 +27,9 @@ const ExibicaoScreen = ({ route }) => {
         </TouchableOpacity>
       </View>
 
-      <Text>Pronto em 3 - 5 min</Text>
+      <View style={styles.temp}>
+        <Text style={styles.textTemp}>Pronto em 3 - 5 min</Text>
+      </View>
 
       <View style={styles.main}>
         <View style={styles.textTopMainContainer}>
@@ -51,26 +58,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F4DEAA",
   },
+  imageHeader: {
+    height: "25%",
+    width: "100%",
+  },
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
+    position: "absolute",
     paddingTop: 30,
     paddingHorizontal: 30,
-    backgroundColor: "#BB5104",
-    height: "25%"
+    height: "25%",
+    width: "100%"
   },
   title: {
     color: "#F4DEAA",
     fontSize: 18,
   },
+  temp: {
+    justifyContent: "center",
+    alignItems: "center",
+    //position: "absolute"
+  },
+  textTemp: {
+    backgroundColor: "#fff",
+    fontWeight: "700",
+    fontSize: 15,
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    borderRadius: 10
+  },
   main: {
     backgroundColor: "#8572FF",
     height: "75%",
     borderTopRightRadius: 40,
-    borderTopLeftRadius: 40
+    borderTopLeftRadius: 40,
+    paddingVertical: 25,
+    paddingHorizontal: 30,
   },
   textTopMainContainer: {
-    
+
   },
   name: {
     fontSize: 20,
