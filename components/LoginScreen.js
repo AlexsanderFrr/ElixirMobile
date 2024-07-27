0d410054580475260cb7db6c61a8cf6b95381ee0
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-26 21:39:49 -0300

    useContext alt

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { View, Text, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, } from 'react-native';
 import css from './styles';
 
