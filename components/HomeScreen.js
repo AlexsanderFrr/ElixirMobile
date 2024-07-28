e1b277f019d747e3ad164033c2d1b1b735616126
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-07-28 15:03:27 -0300

    update fontWeight adição de

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
     height: 100,
   },
   juiceNameHorizontal: {
-    fontWeight: 500,
+    fontWeight: "500",
     fontSize: 16,
     marginTop: 5,
     textAlign: "center",
