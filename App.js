f5883901805daec374e55972c36efe801bc97568
Rafael Oliveira <rafael.oliveira.sss@gmail.com>
2024-07-31 21:40:32 -0300

    arrumando conflito de merge

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import Routes from "./src/routes";
+import Routes from "./src/routes/Router";
+import { AuthProvider } from "./src/context/authContext";
 import store from './src/store/store';
 import { Provider } from 'react-redux';
 
 export default function App() {
   return (
+    <AuthProvider>
     <Provider store={ store }>
       <Routes/>
     </Provider>
+    </AuthProvider>
   );
 }
