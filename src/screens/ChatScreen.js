bb403be31e0becee35690191dafdb749696999d1
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-12-06 15:19:29 -0300

    rota chatbot alterada

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -17,7 +17,7 @@ const ChatBotScreen = () => {
     // Enviar a pergunta para o backend e obter a resposta
     let botMessage;
     try {
-      const response = await fetch('http://127.0.0.1:5000/chatbot', {
+      const response = await fetch('https://chatbot-elixir.onrender.com/chatbot', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
