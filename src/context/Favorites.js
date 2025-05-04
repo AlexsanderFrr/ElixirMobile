99facf0e63295e068818c44b26ad61de53bc10d3
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2025-05-04 19:03:09 -0300

    Create Favorites.js

---
+++ src/context/Favorites.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const FavoritesContext = createContext()
+FavoritesContext.displayName = "MyFavorites"
+
+export default function FavoritesProvider({ children }) {
+    const [ favorite, setFavorite ] = useState([])
+
+    return(
+        <FavoritesContext.Provider
+            value={{ favorite, setFavorite }}
+        >
+            { children }
+        </FavoritesContext.Provider>
+    )
+}
