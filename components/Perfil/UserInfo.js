ec81099d3e4948ae997f5394f5ddffe025ffd808
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2025-06-21 14:57:31 -0300

    Atualizando dependencias

--- components/Perfil/UserInfo.js
+++ components/Perfil/UserInfo.js
@@ -3,10 +3,10 @@ import { View, Text } from "react-native";
 
 const UserInfo = ({ userInfo }) => (
     <View style={{ alignItems: "center", }}>
-        <Text style={{ fontWeight: "600", fontSize: 26, fontFamily: "HelveticaNeue", marginBottom: 5, marginTop: 10, }}>
+        <Text style={{ fontWeight: "600", fontSize: 26, marginBottom: 5, marginTop: 10, }}>
             {userInfo?.nome || userInfo?.name || "Nome do Usuário"}
         </Text>
-        <Text style={{ fontSize: 20, fontFamily: "HelveticaNeue", color: "#8a8a8a" }}>
+        <Text style={{ fontSize: 20, color: "#8a8a8a" }}>
             {userInfo?.email || "email@dominio.com"}
         </Text>
     </View>
