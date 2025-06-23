a2701e29583adef5374541df75ec4fe032144445
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2025-06-23 12:28:55 -0300

    rodando nativo

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Routes from "./src/routes/Router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from "./src/context/authContext";
 import store from './src/store/store';
 import { Provider } from 'react-redux';
@@ -7,9 +8,11 @@ import { Provider } from 'react-redux';
 export default function App() {
   return (
     <AuthProvider>
+      <SafeAreaProvider>
     <Provider store={ store }>
       <Routes/>
     </Provider>
+    </SafeAreaProvider>
     </AuthProvider>
   );
 }
